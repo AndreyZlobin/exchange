75ae15792d22f2f31a7aa4312fbe5d023934640e
Zlobin Andrey <zlobin.andy@gmail.com>
2023-03-10 07:10:56 +0300

    feat: User and oser update

--- src/core/user/types/user.ts
+++ src/core/user/types/user.ts
@@ -3,5 +3,8 @@ import { User as ModelUser, UserSettings as ModelUserSettings } from '@prisma/cl
 export type UserSettings = ModelUserSettings;
 export type UserSettingsWithExcludedFields = Omit<UserSettings, 'id' | 'userId'>;
 export type User = ModelUser & { settings: UserSettings };
-export type UserWithExcludedFields = Omit<User, 'id' | 'deleted' | 'createdAt' | 'settings'>;
+export type UserWithExcludedFields = Omit<
+  User,
+  'id' | 'deleted' | 'createdAt' | 'settings' | 'isWork'
+>;
 export type UserWithoutPassword = Omit<User, 'password'>;
